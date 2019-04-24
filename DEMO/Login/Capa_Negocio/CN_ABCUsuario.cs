575288958fea6bc46ace233bf2e3b007304507bc
Andre Ibarra Perez <RayAsm1@hotmail.com>
2019-04-24 12:18:06 -0500

    Se remueve encriptación, para retornar con ella en el futuro

--- DEMO/Login/Capa_Negocio/CN_ABCUsuario.cs
+++ DEMO/Login/Capa_Negocio/CN_ABCUsuario.cs
@@ -15,7 +15,7 @@ namespace Capa_Negocio
         string key = "mikey";
         public void RegistrarUsuario(String nombre, String apellidos, String contra, String cargo, String nickname, String correo)
         {
-            _ABCUsuario.RegistrarUsuario(nombre,apellidos,Encriptar(contra),cargo,nickname,correo);
+            _ABCUsuario.RegistrarUsuario(nombre,apellidos,contra,cargo,nickname,correo);
         }
         public string Encriptar(string texto)
         {
@@ -61,43 +61,6 @@ namespace Capa_Negocio
             //se regresa el resultado en forma de una cadena
             return Convert.ToBase64String(ArrayResultado,
             0, ArrayResultado.Length);
-        }
-
-        public string Desencriptar(string textoEncriptado)
-        {
-            byte[] keyArray;
-            //convierte el texto en una secuencia de bytes
-            byte[] Array_a_Descifrar =
-            Convert.FromBase64String(textoEncriptado);
-
-            //se llama a las clases que tienen los algoritmos
-            //de encriptación se le aplica hashing
-            //algoritmo MD5
-            MD5CryptoServiceProvider hashmd5 =
-            new MD5CryptoServiceProvider();
-
-            keyArray = hashmd5.ComputeHash(
-            UTF8Encoding.UTF8.GetBytes(key));
-
-            hashmd5.Clear();
-
-            TripleDESCryptoServiceProvider tdes =
-            new TripleDESCryptoServiceProvider();
-
-            tdes.Key = keyArray;
-            tdes.Mode = CipherMode.ECB;
-            tdes.Padding = PaddingMode.PKCS7;
-
-            ICryptoTransform cTransform =
-            tdes.CreateDecryptor();
-
-            byte[] resultArray =
-            cTransform.TransformFinalBlock(Array_a_Descifrar,
-            0, Array_a_Descifrar.Length);
-
-            tdes.Clear();
-            //se regresa en forma de cadena
-            return UTF8Encoding.UTF8.GetString(resultArray);
         }
     }
 }
--- DEMO/Login/Capa_Negocio/CN_Login.cs
+++ DEMO/Login/Capa_Negocio/CN_Login.cs
@@ -15,42 +15,92 @@ namespace Capa_Negocio
         private CD_Login _CD_Login = new CD_Login();
         public MySqlDataReader IniciarSesion(String nickname,String contraseña)
         {
+            MySqlDataReader leer = _CD_Login.IniciarSesion(nickname, contraseña);
+            return leer;
+        }
+
+        public string Encriptar(string texto)
+        {
             //arreglo de bytes donde guardaremos la llave
             byte[] keyArray;
             //arreglo de bytes donde guardaremos el texto
             //que vamos a encriptar
-            byte[] Arreglo_a_Cifrar = UTF8Encoding.UTF8.GetBytes(nickname);
+            byte[] Arreglo_a_Cifrar =
+            UTF8Encoding.UTF8.GetBytes(texto);
 
             //se utilizan las clases de encriptación
             //provistas por el Framework
             //Algoritmo MD5
-            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+            MD5CryptoServiceProvider hashmd5 =
+            new MD5CryptoServiceProvider();
             //se guarda la llave para que se le realice
             //hashing
-            keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+            keyArray = hashmd5.ComputeHash(
+            UTF8Encoding.UTF8.GetBytes(key));
 
             hashmd5.Clear();
 
             //Algoritmo 3DAS
-            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            TripleDESCryptoServiceProvider tdes =
+            new TripleDESCryptoServiceProvider();
 
             tdes.Key = keyArray;
             tdes.Mode = CipherMode.ECB;
             tdes.Padding = PaddingMode.PKCS7;
 
             //se empieza con la transformación de la cadena
-            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            ICryptoTransform cTransform =
+            tdes.CreateEncryptor();
 
             //arreglo de bytes donde se guarda la
             //cadena cifrada
-            byte[] ArrayResultado = cTransform.TransformFinalBlock(Arreglo_a_Cifrar, 0, Arreglo_a_Cifrar.Length);
+            byte[] ArrayResultado =
+            cTransform.TransformFinalBlock(Arreglo_a_Cifrar,
+            0, Arreglo_a_Cifrar.Length);
+
             tdes.Clear();
+
             //se regresa el resultado en forma de una cadena
-            //Convert.ToBase64String(ArrayResultado, 0, ArrayResultado.Length)
-            MySqlDataReader leer = _CD_Login.IniciarSesion(nickname, contraseña);
-            return leer;
+            return Convert.ToBase64String(ArrayResultado,
+            0, ArrayResultado.Length);
         }
 
+        public string Desencriptar(string textoEncriptado)
+        {
+            byte[] keyArray;
+            //convierte el texto en una secuencia de bytes
+            byte[] Array_a_Descifrar =
+            Convert.FromBase64String(textoEncriptado);
+
+            //se llama a las clases que tienen los algoritmos
+            //de encriptación se le aplica hashing
+            //algoritmo MD5
+            MD5CryptoServiceProvider hashmd5 =
+            new MD5CryptoServiceProvider();
+
+            keyArray = hashmd5.ComputeHash(
+            UTF8Encoding.UTF8.GetBytes(key));
+
+            hashmd5.Clear();
+
+            TripleDESCryptoServiceProvider tdes =
+            new TripleDESCryptoServiceProvider();
+
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform =
+            tdes.CreateDecryptor();
+
+            byte[] resultArray =
+            cTransform.TransformFinalBlock(Array_a_Descifrar,
+            0, Array_a_Descifrar.Length);
+
+            tdes.Clear();
+            //se regresa en forma de cadena
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
 
 
     }

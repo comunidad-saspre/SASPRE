d63be746b77ce55b3dcc0fdf8ba3b912561d6791
Macoy <marco_97@live.com.mx>
2019-09-22 13:48:02 -0500

    Modificación de la UI y opción de borrar ultimo elemento y borrar todos

--- DEMO/Login/Capa_Presentacion/Graficos.Designer.cs
+++ DEMO/Login/Capa_Presentacion/Graficos.Designer.cs
@@ -31,13 +31,11 @@
             this.components = new System.ComponentModel.Container();
             this.Posicion = new System.Windows.Forms.Timer(this.components);
             this.panelPrincipal = new Capa_Presentacion.MyPanel();
-            this.tomatepng = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.panelControles = new Capa_Presentacion.MyPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.panelPrincipal.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.tomatepng)).BeginInit();
+            this.panelControles.SuspendLayout();
             this.SuspendLayout();
             // 
             // Posicion
@@ -48,11 +46,7 @@
             // 
             // panelPrincipal
             // 
-            this.panelPrincipal.Controls.Add(this.tomatepng);
-            this.panelPrincipal.Controls.Add(this.label2);
-            this.panelPrincipal.Controls.Add(this.label1);
-            this.panelPrincipal.Controls.Add(this.button1);
-            this.panelPrincipal.Controls.Add(this.checkBox1);
+            this.panelPrincipal.BackColor = System.Drawing.Color.Silver;
             this.panelPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelPrincipal.Location = new System.Drawing.Point(0, 0);
             this.panelPrincipal.Name = "panelPrincipal";
@@ -60,39 +54,40 @@
             this.panelPrincipal.TabIndex = 4;
             this.panelPrincipal.Paint += new System.Windows.Forms.PaintEventHandler(this.panelPrincipal_Paint);
             // 
-            // tomatepng
+            // panelControles
             // 
-            this.tomatepng.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.tomatepng.Image = global::Capa_Presentacion.Properties.Resources.TOMATE;
-            this.tomatepng.Location = new System.Drawing.Point(653, 81);
-            this.tomatepng.Name = "tomatepng";
-            this.tomatepng.Size = new System.Drawing.Size(70, 65);
-            this.tomatepng.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.tomatepng.TabIndex = 4;
-            this.tomatepng.TabStop = false;
-            this.tomatepng.DragEnter += new System.Windows.Forms.DragEventHandler(this.tomatepng_DragEnter);
+            this.panelControles.Controls.Add(this.button2);
+            this.panelControles.Controls.Add(this.button3);
+            this.panelControles.Controls.Add(this.button1);
+            this.panelControles.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelControles.Location = new System.Drawing.Point(677, 0);
+            this.panelControles.Name = "panelControles";
+            this.panelControles.Size = new System.Drawing.Size(241, 579);
+            this.panelControles.TabIndex = 7;
             // 
-            // label2
+            // button2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(361, 21);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(170, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "GFAFICACIÓN EN TIEMPO REAL";
+            this.button2.Location = new System.Drawing.Point(82, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Borrar ultimo tomate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label1
+            // button3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(668, 337);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.button3.Location = new System.Drawing.Point(82, 114);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 38);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Borrar todo de panel principal";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(653, 21);
+            this.button1.Location = new System.Drawing.Point(82, 26);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(123, 38);
             this.button1.TabIndex = 0;
@@ -100,28 +95,17 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(671, 203);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
             // Graficos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(918, 579);
+            this.Controls.Add(this.panelControles);
             this.Controls.Add(this.panelPrincipal);
             this.Name = "Graficos";
             this.Text = "Graficos";
             this.Load += new System.EventHandler(this.Graficos_Load);
-            this.panelPrincipal.ResumeLayout(false);
-            this.panelPrincipal.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.tomatepng)).EndInit();
+            this.panelControles.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -129,11 +113,10 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private MyPanel panelPrincipal;
-        private System.Windows.Forms.PictureBox tomatepng;
         private System.Windows.Forms.Timer Posicion;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private MyPanel panelControles;
     }
 }
--- DEMO/Login/Capa_Presentacion/Graficos.cs
+++ DEMO/Login/Capa_Presentacion/Graficos.cs
@@ -19,10 +19,7 @@ namespace Capa_Presentacion
 
         private void Graficos_Load(object sender, EventArgs e)
         {
-            ControlExtension.Draggable(checkBox1, true);
-            ControlExtension.Draggable(label1, true);
             ControlExtension.Draggable(button1, true);
-            ControlExtension.Draggable(tomatepng, true);
         }
 
         private void panelPrincipal_Paint(object sender, PaintEventArgs e)
@@ -33,8 +30,8 @@ namespace Capa_Presentacion
         private void Posicion_Tick(object sender, EventArgs e)
         {
 
-            Point locationOnForm = tomatepng.FindForm().PointToClient(tomatepng.Parent.PointToScreen(tomatepng.Location));
-            tomatepng.Location = new Point(locationOnForm.X, locationOnForm.Y);
+            //Point locationOnForm = tomatepng.FindForm().PointToClient(tomatepng.Parent.PointToScreen(tomatepng.Location));
+            //tomatepng.Location = new Point(locationOnForm.X, locationOnForm.Y);
             //MessageBox.Show("X:"+tomatepng.Location.X.ToString()); /*Aquí para obtener la localización de X*/
             //MessageBox.Show("Y:" + tomatepng.Location.Y.ToString()); /*Aqúí para obtener la localización de Y*/
         }
@@ -45,9 +42,10 @@ namespace Capa_Presentacion
 
         int i = 0;
         int NoTomate = 0;
+        PictureBox pb;
         private void button1_Click(object sender, EventArgs e)
         {
-            PictureBox pb = new PictureBox();
+            pb= new PictureBox();
             pb.Name = "tomate" + NoTomate++;
             pb.SizeMode = PictureBoxSizeMode.Zoom;
             pb.Image = Properties.Resources.TOMATE;
@@ -57,6 +55,16 @@ namespace Capa_Presentacion
             this.panelPrincipal.Controls.Add(pb);
             ControlExtension.Draggable(pb , true);
             i += 60;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.panelPrincipal.Controls.Remove(pb);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.panelPrincipal.Controls.Clear();
         }
     }
 }

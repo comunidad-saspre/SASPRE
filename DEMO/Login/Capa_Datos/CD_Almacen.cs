ee0275b24e5a83a2f0a87bce5ba73195d3977e66
Andre Ibarra Perez <RayAsm1@hotmail.com>
2019-10-08 23:01:57 -0500

    Cambios de clima

--- DEMO/Login/Capa_Datos/CD_Almacen.cs
+++ DEMO/Login/Capa_Datos/CD_Almacen.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace Capa_Datos
 {
     class CD_Almacen
     {
+        private CD_ConexionBD conexion = new CD_ConexionBD();
+        MySqlDataReader leer;
+        DataTable tablaAlmacen = new DataTable();
+        MySqlCommand comando;
     }
 }

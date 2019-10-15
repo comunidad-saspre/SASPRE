6ab707d0940665a4eae5840b22875ecc47a4604e
Andre Ibarra Perez <RayAsm1@hotmail.com>
2019-10-14 22:22:33 -0500

    Más climas

--- DEMO/Login/Capa_Presentacion/Program.cs
+++ DEMO/Login/Capa_Presentacion/Program.cs
@@ -26,7 +26,7 @@ namespace Capa_Presentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
+            Application.Run(new Login());
             
         }
     }

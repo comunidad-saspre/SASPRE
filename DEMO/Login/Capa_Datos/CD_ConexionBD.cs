db96641592d78d036ddb07314a20852a0dd1fd7d
MIGUELGM98 <gonzalez.martinez.16040@itsmante.edu.mx>
2019-10-21 12:06:15 -0500

    BARRA

--- DEMO/Login/Capa_Datos/CD_ConexionBD.cs
+++ DEMO/Login/Capa_Datos/CD_ConexionBD.cs
@@ -13,11 +13,12 @@ namespace Capa_Datos
     {
         //MySqlConnection Conexion = new MySqlConnection("Server = localhost; Port = 3306; Database = saspre; Uid = root; Pwd = oscarraul");
         //MySqlConnection Conexion = new MySqlConnection("Server = localhost; Port = 3306; Database = saspre; Uid = root; Pwd = admin");
-        MySqlConnection Conexion = new MySqlConnection("Server = localhost; Port = 3320; Database = saspre; Uid = root; Pwd = muski");
+        // MySqlConnection Conexion = new MySqlConnection("Server = localhost; Port = 3320; Database = saspre; Uid = root; Pwd = muski");
         // MySqlConnection Conexion = new MySqlConnection("Server = localhost; Port = 3306; Database = saspre; Uid = root; Pwd = 1234");
         //MySqlConnection Conexion = new MySqlConnection("Server = localhost; Port = 3320; Database = saspre; Uid = root; Pwd = fili");
         //MySqlConnection Conexion = new MySqlConnection("Server = localhost; Port = 3306; Database = saspre; Uid = root; Pwd = root");
         //MySqlConnection Conexion = new MySqlConnection("Server = localhost; Port = 3320; Database = saspre; Uid = root; Pwd = admin");
+        MySqlConnection Conexion = new MySqlConnection("Server = localhost; Port = 3320; Database = saspre; Uid = root; Pwd = papasote2121");
 
         public MySqlConnection AbrirConexion()
         {
